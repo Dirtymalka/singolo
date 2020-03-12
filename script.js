fc2e3cdb4466e5d4d0454b1e9fc7b7d5ab75e95e
Artem <artem.antonov1993@gmail.com>
2020-03-13 01:40:11 +0300

    feat: add switching sliders in script.js

--- script.js
+++ script.js
@@ -15,3 +15,25 @@ display.addEventListener('click', (event) => {
     if (display.querySelector('.black-horizontal').classList.contains('block') && event.target.classList.contains('horizontal-phone')) display.querySelector('.black-horizontal').classList.remove('block');
     else if (!display.querySelector('.black-horizontal').classList.contains('block') && event.target.classList.contains('black-horizontal') || event.target.classList.contains('horizontal-phone')) display.querySelector('.black-horizontal').classList.add('block');
 });
+
+
+
+
+
+
+const container = document.querySelector('.slider-container');
+const slider = container.querySelector('.slider__content');
+
+container.addEventListener('click', (event) => {
+    if (event.target.classList.contains('right-arrow') || event.target.classList.contains('left-arrow')) {
+        slider.querySelectorAll('div').forEach(element => element.classList.toggle('hidden'));
+        if (!slider.querySelector('.slider2').classList.contains('hidden')) {
+            container.classList.add('slider-bg');
+            container.querySelectorAll('button').forEach(element => element.classList.add('slider-bg'));
+        }
+        else {
+            container.classList.remove('slider-bg');
+            container.querySelectorAll('button').forEach(element => element.classList.remove('slider-bg'));
+        }
+    }
+});
