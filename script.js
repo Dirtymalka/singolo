2f1bbb741d67112c089eb14f3df12d85ed5b89d1
Artem <artem.antonov1993@gmail.com>
2020-03-14 01:08:44 +0300

    feat: add PopUp

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const navigation = document.querySelector('.navigation');
 const display = document.querySelector('.slider__content');
 
+//Intaractive Menu
+
 navigation.addEventListener('click', (event) => {
     navigation.querySelectorAll('a').forEach(element => element.classList.remove('active'));
     event.target.classList.add('active');
-}, true);
+});
+
+//Disconnecting Phones
 
 display.addEventListener('click', (event) => {
     if (display.querySelector('.black-vertical').classList.contains('block') && event.target.classList.contains('vertical-phone')) display.querySelector('.black-vertical').classList.remove('block');
@@ -17,9 +21,7 @@ display.addEventListener('click', (event) => {
 });
 
 
-
-
-
+//Slider
 
 const container = document.querySelector('.slider-container');
 const slider = container.querySelector('.slider__content');
@@ -36,4 +38,36 @@ container.addEventListener('click', (event) => {
             container.querySelectorAll('button').forEach(element => element.classList.remove('slider-bg'));
         }
     }
+});
+
+//Form
+
+const submit = document.getElementById('submit');
+const closeButton = document.getElementById('close-button');
+const messageTheme = document.getElementById('no-theme').innerHTML;
+const messageDescription = document.getElementById('no-description').innerHTML;
+
+submit.addEventListener('click', () => {
+    const subject = document.getElementById('subject').value.toString();
+    if (subject != '') {
+        document.getElementById('message-theme').innerText = subject;
+    } else {
+        document.getElementById('no-theme').innerHTML = 'Нет темы';
+    }
+
+    const projectDescription = document.getElementById('project-description').value.toString();
+    if (projectDescription != '') {
+        document.getElementById('message-description').innerText = projectDescription;
+    } else {
+        document.getElementById('no-description').innerHTML = 'Нет описания';
+    }
+
+    document.getElementById('message-block').classList.remove('hidden-block');
+});
+
+closeButton.addEventListener('click', () => {
+    document.getElementById('message-block').classList.add('hidden-block');
+    document.getElementById('no-theme').innerHTML = messageTheme;
+    document.getElementById('no-description').innerHTML = messageDescription;
+    document.getElementById('form').reset();
 });
